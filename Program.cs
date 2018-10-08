90378c44415862e89e3693fbbf2e6f06b32aa4f3
Nicholas Landau <nrlandau@gmail.com>
2018-10-08 14:01:05 -0400

    Added volume output

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/* Lab 2 by Nicholas Landau
+This program takes a length and a width of a room and calculates the area and perimeter of it.
+ */
+using System;
 
 namespace Lab2
 {
@@ -9,7 +12,10 @@ namespace Lab2
             //Var Dec
             float length;
             float width;
+            float hight;
             float area;
+            float surfaceArea;
+            float volume;
             float perimeter;
             char repeat;
             //Main Loop
@@ -19,19 +25,22 @@ namespace Lab2
                 length = float.Parse(System.Console.ReadLine());
                 System.Console.WriteLine("Input the Width of the room:");
                 width = float.Parse(System.Console.ReadLine());
+                System.Console.WriteLine("Input the Hight of the room:");
+                hight = float.Parse(System.Console.ReadLine());
             //Logic
                 area = length * width;
                 perimeter = length * 2.0f + width * 2.0f;
+                volume = length * width * hight;
+                surfaceArea = length * width * 2.0f + width * hight * 2.0f + length * hight * 2.0f;
             //Output
                 System.Console.WriteLine("The area is ({0}) and the perimeter is ({1})",area,perimeter);
+                System.Console.WriteLine("The volume of the room is ({0}) and the surface area is ({1})",volume,surfaceArea);
 
-                System.Console.WriteLine("Do you want to repeat?(Y/?");
-                repeat = char.Parse(System.Console.ReadLine());
+                System.Console.WriteLine("Do you want to repeat?(Y/?)");
+                repeat = System.Console.ReadLine()[0];
                 if (repeat != 'y' && repeat != 'Y')
                     break; 
-
             }
-
         }
     }
 }
